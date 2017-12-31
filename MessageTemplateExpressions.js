0478f08b1012002ea7f7a8eb36c0fd22e14ecdde
Compilenix <Compilenix@compilenix.org>
2017-12-31 03:16:20 +0100

    fix nginx default filter expression

--- MessageTemplateExpressions.js
+++ MessageTemplateExpressions.js
@@ -1,6 +1,6 @@
 // Nginx
 // log_format main '[$time_local] status:$status request_time:$request_time upstream_response_time:$upstream_response_time bytes_sent:$body_bytes_sent client_ip:$remote_addr domain:$host request:"$request" referer:"$http_referer" user_agent:"$http_user_agent"';
-defaultMessageTemplateFilter = /^\[((\d{1,2}\/\w{3}\/\d{4}):(\d{2}:\d{2}:\d{2}) ([+-]\d{4}))\] status:(\d{3}) request_time:(\d{1,}.\d{3}) upstream_response_time:(\d{1,}.\d{3}) bytes_sent:(\d{1,}) client_ip:(\b\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}\b|(:{0,2}[\da-f]{1,4}){1,8}) domain:([\w.-]*) request:"((\w{3,8}) (\/[\w-.,_~:<>\\/[\]%@!$'()*+;?=&#]*) HTTP\/(\d\.\d))" referer:"([\w-.,_~:<>\\/[\]%@!$'()*+;?=&#]*)" user_agent:"([\w-.,_~: \/[\]%@!$'()*+;?=&#]*)"$/;
+defaultMessageTemplateFilter = /^\[((\d{1,2}\/\w{3}\/\d{4}):(\d{2}:\d{2}:\d{2}) ([+-]\d{4}))\] status:(\d{3}) request_time:(\d{1,}.\d{3}) upstream_response_time:((\d{1,}.\d{3}|-)) bytes_sent:(\d{1,}) client_ip:(\b\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}\b|(:{0,2}[\da-f]{1,4}){1,8}) domain:([\w.-]*) request:"((\w{3,8}) (\/[\w-.,_~:<>\\/[\]%@!$'()*+;?=&#]*) HTTP\/(\d\.\d))" referer:"([\w-.,_~:<>\\/[\]%@!$'()*+;?=&#]*)" user_agent:"([\w-.,_~: \/[\]%@!$'()*+;?=&#]*)"$/;
 defaultMatchingGroupName = {
     All: 0,
     DateTime: 1,
@@ -10,15 +10,15 @@ defaultMatchingGroupName = {
     StatusCode: 5,
     RequestTime: 6,
     UpstreamResponseTime: 7,
-    BytesSent: 8,
-    ClientIp: 9,
-    Domain: 11,
-    Request: 12,
-    Method: 13,
-    Path: 14,
-    ProtocolVersion: 15,
-    Referer: 16,
-    UserAgent: 17
+    BytesSent: 9,
+    ClientIp: 10,
+    Domain: 12,
+    Request: 13,
+    Method: 14,
+    Path: 15,
+    ProtocolVersion: 16,
+    Referer: 17,
+    UserAgent: 18
 };
 
 // Test strings
@@ -44,6 +44,7 @@ defaultMatchingGroupName = {
 // [10/Dec/2017:01:08:21 +0100] status:200 request_time:5.060 upstream_response_time:5.060 bytes_sent:521 client_ip:1.22.33.44 domain:example.com request:"GET / HTTP/2.0" referer:"https://example.com/page/1" user_agent:""
 // [10/Dec/2017:01:08:21 +0100] status:200 request_time:5.060 upstream_response_time:5.060 bytes_sent:521 client_ip:1.22.33.44 domain:example.com request:"GET / HTTP/2.0" referer:"-" user_agent:"curl/7.53.1"
 // [10/Dec/2017:01:08:21 +0100] status:200 request_time:5.060 upstream_response_time:5.060 bytes_sent:521 client_ip:1.22.33.44 domain:example.com request:"GET / HTTP/2.0" referer:"-" user_agent:"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_6) AppleWebKit/604.4.7 (KHTML, like Gecko) Version/11.0.2 Safari/604.4.7"
+// [10/Dec/2017:01:08:21 +0100] status:404 request_time:0.000 upstream_response_time:- bytes_sent:0 client_ip:1.22.33.44 domain:example.com request:"GET / HTTP/2.0" referer:"-" user_agent:""
 
 // ----------------------------------------------------
 // Apache
